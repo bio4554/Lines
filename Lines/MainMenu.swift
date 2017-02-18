9cce0bd898ba9fd7c3f30cb4b1e1d74c6968f629
bio4554 <bio4554@bio4554s-MacBook-Air.local>
2017-02-17 20:43:19 -0600

    v1.0

--- Lines/MainMenu.swift
+++ Lines/MainMenu.swift
@@ -27,8 +27,8 @@ class MainMenu: SKScene {
         goText.position = CGPoint(x: size.width/2, y: size.height/4)
         
         let versionText = SKLabelNode()
-        versionText.text = "v1.3 BETA © Mars Dev 2017"
-        versionText.fontSize = 75
+        versionText.text = "v1.0 © Mars Dev 2017"
+        versionText.fontSize = 100
         //versionText.fontName = "AvenirNext-Bold"
         versionText.position = CGPoint(x: size.width/2, y: 100)
         
